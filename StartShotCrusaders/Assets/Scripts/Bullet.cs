2d5c110d3d672e68770b31250a5c8447a26a83cd
GreyZar37 <52458016+GreyZar37@users.noreply.github.com>
2021-03-01 10:59:26 +0100

    PlayerUpdate

---
+++ StartShotCrusaders/Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public static int playerDamage;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+
+        {
+
+            Destroy(gameObject);
+           // other.gameObject.GetComponent<EnemyHealth>().TakeDamage(playerDamage);
+
+        }
+
+    }
+}
---
+++ StartShotCrusaders/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float bulletForce = 20f;
+
+
+    float currentTimer;
+    public float playerCoolDowntimer = 1.5f;
+
+
+    void Update()
+    {
+        currentTimer -= Time.deltaTime;
+
+        if (currentTimer <= 0)
+        {
+            currentTimer = 0;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && currentTimer == 0)
+        {
+            shoot();
+            currentTimer = playerCoolDowntimer;
+        }
+
+        void shoot()
+        {
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+
+
+        }
+    }
+}
