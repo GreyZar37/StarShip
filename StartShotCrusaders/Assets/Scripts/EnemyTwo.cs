4a0fe6935cd4b3a22026c0767e216803b4eb3b43
MeisterRepsak <71341417+MeisterRepsak@users.noreply.github.com>
2021-03-03 12:09:40 +0100

    rework

---
+++ StartShotCrusaders/Assets/Scripts/EnemyTwo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTwo : MonoBehaviour
+{
+    public GameObject Bomb;
+    private Vector2 endPos;
+    public GameObject testPlayer;
+    void Awake()
+    {
+        endPos = testPlayer.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
