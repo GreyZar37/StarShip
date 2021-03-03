1c7438bd28b756d70d8f76cc4fc18c4f8f94da57
MeisterRepsak <71341417+MeisterRepsak@users.noreply.github.com>
2021-03-03 11:22:39 +0100

    Niggerama enemyOne

--- StartShotCrusaders/Assets/Scripts/EnemyOne.cs
+++ StartShotCrusaders/Assets/Scripts/EnemyOne.cs
@@ -5,22 +5,69 @@ using UnityEngine;
 public class EnemyOne : MonoBehaviour
 {
     private Vector2 limitYPos = new Vector2(0, 3.5f);
+    public Transform shootPoint;
+    public Transform testPlayer;
+    public GameObject bullet;
+
+    private float fireRate;
+    public const float constfireRate = 0.25f;
+
+    public float shootTime = 5f;
+    bool canShoot;
+    bool hasSetAngle;
     void Start()
     {
-        
+        shootTime = 5f;
+        fireRate = constfireRate;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.y > 3.5f)
+        fireRate -= Time.deltaTime;
+        if(transform.position.y > 3.5f && !hasSetAngle)
         {
-           transform.position = new Vector2(0, Mathf.Lerp(transform.position.y, limitYPos.y, 8 * Time.deltaTime));
+           transform.position = new Vector2(0, Mathf.Lerp(transform.position.y, 3.5f, 8 * Time.deltaTime));
         }
+        if(shootTime > 0f)
+        {
+            if (transform.position.y < 3.51f)
+            {
+                canShoot = fireRate < 0;
+                if (canShoot)
+                {
+                    fireRate = constfireRate;
+                    ShootBullet();
+                }
+                shootTime -= Time.deltaTime;
 
-        if( transform.position.y < 3.51f)
-        {
-            print("PEW PEW!");
+            }
         }
+        else
+        {
+            if (!hasSetAngle)
+            {
+                Vector2 dir = new Vector2(transform.position.x - testPlayer.position.x, transform.position.y - testPlayer.position.y);
+                hasSetAngle = true;
+                transform.up = dir;
+                
+            }
+            transform.Translate(transform.up * -9f * Time.deltaTime);
+        }
+        
+        if(transform.position.y < -7f)
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+
+    void ShootBullet()
+    {
+        Vector2 dir = new Vector2(shootPoint.position.x - testPlayer.position.x, shootPoint.position.y - testPlayer.position.y);
+
+        shootPoint.up = dir;
+
+        Instantiate(bullet, shootPoint.position, Quaternion.Euler(0,0,180 + shootPoint.rotation.eulerAngles.z));
     }
 }
